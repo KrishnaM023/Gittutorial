9a08df0c17d6f269e69679c750767afd1f91d0a3
KrishnaM023 <krishna050120>
2024-01-07 20:49:25 +0530

    AxiosUpdateUser Commit

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
 const USERLIST = document.querySelector('#users');
-let arr = [];
+let arr = []; // Initialize the array globally
 
 document.addEventListener('DOMContentLoaded', () => {
   getStoredUserData();
@@ -26,15 +26,28 @@ function onsubmit(e) {
       arr = [];
     }
 
-    axios
-      .post("https://crudcrud.com/api/551361f523c14cee9a512d8c78f05479/appointmentdata", userData)
-      .then((response) => {
-        showNewUserOnScreen(response.data);
-      })
-      .catch((err) => {
-        document.body.innerHTML = document.body.innerHTML + "<h4>Something went wrong</h4>"
-        console.log(err);
-      })
+    if (!userData._id) {
+      // If _id is not present, it's a new user, so perform POST operation
+      axios
+        .post("https://crudcrud.com/api/551361f523c14cee9a512d8c78f05479/appointmentdata", userData)
+        .then((response) => {
+          showNewUserOnScreen(response.data);
+        })
+        .catch((err) => {
+          document.body.innerHTML = document.body.innerHTML + "<h4>Something went wrong</h4>"
+          console.log(err);
+        });
+    } else {
+      // If _id is present, it's an existing user, so perform PUT operation
+      axios
+        .put(`https://crudcrud.com/api/551361f523c14cee9a512d8c78f05479/appointmentdata/${userData._id}`, userData)
+        .then(() => {
+          updateList();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
 
     nameInput.value = '';
     emailInput.value = '';
@@ -48,12 +61,19 @@ function updateList() {
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(`${userData.name} : ${userData.email}`));
 
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.addEventListener('click', () => {
+      editUserData(index);
+    });
+
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.addEventListener('click', () => {
       deleteUserData(userData._id, index);
     });
 
+    li.appendChild(editButton);
     li.appendChild(deleteButton);
 
     USERLIST.appendChild(li);
@@ -82,6 +102,47 @@ function deleteUserData(userId, index) {
     .catch((err) => {
       console.log(err);
     });
+}
+
+function editUserData(index) {
+  const userData = arr[index];
+  nameInput.value = userData.name;
+  emailInput.value = userData.email;
+
+  // Set the _id property in the userData object so that it will be treated as an existing user during form submission
+  nameInput.userDataId = userData._id;
+
+  // Change the form submit button text to indicate editing
+  myForm.querySelector('button[type="submit"]').innerText = 'Edit User';
+
+  myForm.addEventListener('submit', function onEditSubmit(e) {
+    e.preventDefault();
+
+    // Update the userDataArray with the edited data
+    userData.name = nameInput.value;
+    userData.email = emailInput.value;
+
+    if (userData._id) {
+      // If _id is present, it's an existing user, so perform PUT operation
+      axios
+        .put(`https://crudcrud.com/api/551361f523c14cee9a512d8c78f05479/appointmentdata/${userData._id}`, userData)
+        .then(() => {
+          updateList();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    // Clear the form fields and reset the form submit button text
+    nameInput.value = '';
+    emailInput.value = '';
+    nameInput.userDataId = null;
+    myForm.querySelector('button[type="submit"]').innerText = 'Add User';
+
+    // Remove the temporary event listener
+    myForm.removeEventListener('submit', onEditSubmit);
+  });
 }
 
 function showNewUserOnScreen(user) {
